f6ffefc9627ffbf5951fbde74fbae8b4780cb4b5
Xander Chua <55899838+xander-cs@users.noreply.github.com>
2023-12-14 02:48:08 -0500

    most of the backend integration

--- src/App.js
+++ src/App.js
@@ -1,78 +1,141 @@
 import React from 'react';
 import './App.css';
 import web3 from './web3';
+import TicketSale from './contract';
 
-function App() 
+class App extends React.Component
 {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>// T1CKET SALE</h1>
-      </header>
-      
-      <hr />
+  state = {
+    accounts: [],
+    ticketID: '',
+    getAddress: '',
+    returnAddress: '',
+    newOwnerAddress: '',
+    offerAddress: '',
+    acceptAddress: ''
+  };
 
-      <body>
-        <div>
-          <h2>Tickets</h2>
+  async componentDidMount()
+  {
+    const accounts = await web3.eth.getAccounts();
+    const ticketID = 0;
+    const getAddress = '';
+    const returnAddress = '';
+    const newOwnerAddress = '';
+    const offerAddress = '';
+    const acceptAddress = '';
 
-          <div>
-            <h3>Buy Ticket</h3>
+    this.setState({accounts, ticketID, getAddress, returnAddress, newOwnerAddress, offerAddress, acceptAddress});
+  }
 
-            <label>Ticket ID: </label>
-            <input></input>
-            <button>Purchase</button>
-          </div>
+  onPurchase = async () =>
+  {
+    await TicketSale.methods.buyTicket(this.state.ticketID).send({
+      from: this.state.accounts[0],
+      value: TicketSale.methods.ticketPrice().call()
+    });
+  };
+  
+  onGet = async () =>
+  {
+    await TicketSale.methods.getTicketOf(this.state.getAddress).call();
+  }
 
-          <div>
-            <h3>Get Ticket</h3>
+  onReturn = async () =>
+  {
+    await TicketSale.methods.returnTicket(this.state.returnAddress).send({
+      from: this.state.accounts[0],
+      value: TicketSale.methods.ticketPrice().call()
+    });
+  }
 
-            <label>From address: </label>
-            <input />
-            <button>Get</button>
-          </div>
+  onChangeOwner = async () =>
+  {
+    await TicketSale.methods.changeOwner(this.state.newOwnerAddress).call();
+  }
 
-          <div>
-            <h3>Return Ticket</h3>
+  onOffer = async () =>
+  {
+    await TicketSale.methods.offerSwap(this.state.offerAddress).call();
+  }
 
-            <label>To address: </label>
-            <input />
-            <button>Return</button>
-          </div>
+  onAccept = async () =>
+  {
+    await TicketSale.methods.acceptSwapOffer(this.state.acceptAddress).call();
+  }
 
-          <div>
-            <h3>Change Ticket Owner</h3>
-
-            <label>Address of new owner: </label>
-            <input />
-            <button>Change</button>
-          </div>
-        </div>
-
+  render()
+  {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>// T1CKET SALE</h1>
+        </header>
+        
         <hr />
 
-        <div>
-          <h2>Swaps</h2>
+        <body>
+          <div>
+            <h2>Tickets</h2>
+
+            <div>
+              <h3>Buy Ticket</h3>
+
+              <label>Ticket ID: </label>
+              <input value={this.state.ticketID} />
+              <button onEnter={this.onPurchase}>Purchase</button>
+            </div>
+
+            <div>
+              <h3>Get Ticket</h3>
+
+              <label>From address: </label>
+              <input value={this.state.getAddress}/>
+              <button onEnter={this.onGet}>Get</button>
+            </div>
+
+            <div>
+              <h3>Return Ticket</h3>
+
+              <label>To address: </label>
+              <input value={this.state.returnAddress}/>
+              <button onEnter={this.onReturn}>Return</button>
+            </div>
+
+            <div>
+              <h3>Change Ticket Owner</h3>
+
+              <label>Address of new owner: </label>
+              <input value={this.state.newOwnerAddress}/>
+              <button onEnter={this.onChangeOwner}>Change</button>
+            </div>
+          </div>
+
+          <hr />
 
           <div>
-            <h3>Offer Swap</h3>
+            <h2>Swaps</h2>
 
-            <label>Send offer to address: </label>  
-            <input />
-            <button>Send</button>
-          </div>
-          
-          <div>
-            <h3>Accept Offer</h3>
+            <div>
+              <h3>Offer Swap</h3>
 
-            <label>Accept from address: </label>  
-            <input />
-            <button>Accept</button>
+              <label>Send offer to address: </label>  
+              <input value={this.state.offerAddress}/>
+              <button onEnter={this.onOffer}>Send</button>
+            </div>
+            
+            <div>
+              <h3>Accept Offer</h3>
+
+              <label>Accept from address: </label>  
+              <input value={this.state.acceptAddress}/>
+              <button onEnter={this.onAccept}>Accept</button>
+            </div>
           </div>
-        </div>
-      </body>
-    </div>
-  );
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
