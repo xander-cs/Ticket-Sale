50c9164cecc2833b8668dd4f95b7b92e1ff049e1
Xander Chua <55899838+xander-cs@users.noreply.github.com>
2023-12-13 23:13:44 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import web3 from './web3';
+
+function App() 
+{
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+
+      <body>
+        <h1>test</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
